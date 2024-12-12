96cf40cbe46e7de59041de9a6e4c86fb88c4c3e3
felixbrun0 <felix.brun6@etu.univ-lorraine.fr>
2024-12-12 16:39:25 +0100

    start patch

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  baseUrl: 'https://morpion-api.edu.netlor.fr/',
-  headers: {
-    'Content-Type': 'application/json',
-  }
-}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  blabla
+</template>
