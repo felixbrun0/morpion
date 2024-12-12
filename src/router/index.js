60515e08b6b35699363566d4692f5946f26a4b0c
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-12-12 18:01:00 +0100

    Exercice 5

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import ProfileView from "@/views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,11 @@ const router = createRouter({
     {
       path: '/games/:id',
       name: 'game',
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
     }
   ],
 })
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,58 @@
+<script>
+import api from '../api';
+
+export default {
+  name: "ProfileView",
+  data() {
+    return {
+      user: {
+        name: '',
+        email: ''
+      },
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    api.get('/api/profile')
+      .then(response => {
+        next(vm => {
+          vm.user = response.data;
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        next();
+      });
+  },
+  methods: {
+    saveProfile() {
+      api.put('/api/profile', this.user)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    },
+    goToHome() {
+      this.$router.push('/home');
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Profile</h1>
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="user.name">
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="user.email">
+      <button type="submit" @click="saveProfile">Enregistrer</button>
+    </form>
+    <button @click="goToHome">Retour à l'accueil</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
