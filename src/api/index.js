214f5fa5363abb219883bd5de888827de1996d18
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-12-12 18:00:50 +0100

    fix api key error

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://morpion-api.edu.netlor.fr",
   headers: {
     "Content-type": "application/json",
-    "Authorization": `key=${import.meta.env.API_KEY}`
+    "Authorization": `key=${import.meta.env.VITE_API_KEY}`
   }
 })
